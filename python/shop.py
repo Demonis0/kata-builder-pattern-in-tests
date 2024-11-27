34f424e50c0b2e4dec8a4131e6e12719005539f0
Hugo PANEL <hugopanel@gmail.com>
2024-11-27 10:53:06 +0100

    Test builder pattern

--- python/shop.py
+++ python/shop.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 
@@ -19,11 +20,83 @@ class User:
     verified: bool
     
     def __init__(self):
-        self.name = "bob"
+        self.name = "Bob"
         self.email = "bob@domain.tld"
-        self.age = 0
-        self.address = Address("", "", "", "", "")
-        self.verified = False
+        self.age = 20
+        self.address = Address("51 Franklin Street", "Fifth Floor", "Boston", "02110", "USA")
+        self.verified = True
+
+
+## Source aide builder pattern en python : https://refactoring.guru/design-patterns/builder/python/example
+class UserBuilder(ABC):
+    @property
+    @abstractmethod
+    def user(self) -> None:
+        pass
+
+    @abstractmethod
+    def underage(self) -> None:
+        pass
+
+    @abstractmethod
+    def not_verified(self) -> None:
+        pass
+
+    @abstractmethod
+    def foreign(self) -> None:
+        pass
+
+
+class ConcreteUserBuilder(UserBuilder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self):
+        self._user = User()
+
+    @property
+    def user(self) -> User:
+        user = self._user
+        self.reset()
+        return user
+
+    def address_usa(self) -> None:
+        self._user.address = Address("51 Franklin Street", "Fifth Floor", "Boston", "02110", "USA")
+
+    def address_france(self) -> None:
+        self._user.address = Address("33 quai d'Orsay", "", "Paris", "75007", "France")
+
+    def underage(self) -> None:
+        self._user.age = 16
+
+    def not_verified(self) -> None:
+        self._user.verified = False
+
+    def foreign(self) -> None:
+        self._user.address.country = "France"
+
+
+class UserUnderage():
+    def __init__(self) -> None:
+        pass
+
+    def get_user(self) -> User:
+        builder = ConcreteUserBuilder()
+        builder.underage()
+        return builder.user
+
+
+class UserDirector:
+    def __init__(self) -> None:
+        self._builder = None
+
+    @property
+    def builder(self) -> UserBuilder:
+        return self._builder
+    
+    @builder.setter
+    def builder(self, builder: UserBuilder) -> None:
+        self._builder = builder
 
 
 class Shop:
--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -1,35 +1,49 @@
-from shop import Shop, User
+from shop import ConcreteUserBuilder, Shop, User
 
 user = User()
 
-def test_happy_path(fsf_address):
-    user.address = fsf_address
-    user.age = 25
-    user.verified = True
+def test_happy_path():
+    # user.address = fsf_address
+    # user.age = 25
+    # user.verified = True
+
+    user = ConcreteUserBuilder()
+    user = user.user
 
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
-def test_minors_cannot_order_from_the_shop(fsf_address):
-    user.address = fsf_address
-    user.age = 16
-    user.verified = True
+def test_minors_cannot_order_from_the_shop():
+    # user.address = fsf_address
+    # user.age = 16
+    # user.verified = True
+
+    user = ConcreteUserBuilder()
+    user.underage()
+    user = user.user
 
     assert not Shop.can_order(user)
 
 
+def test_cannot_order_if_not_verified():
+    # user.address = fsf_address
+    # user.age = 25
+    # user.verified = False
 
-def test_cannot_order_if_not_verified(fsf_address):
-    user.address = fsf_address
-    user.age = 25
-    user.verified = False
+    user = ConcreteUserBuilder()
+    user.not_verified()
+    user = user.user
 
     assert not Shop.can_order(user)
 
-def test_foreigners_must_be_foreign_fee(paris_address):
-    user.address = paris_address
-    user.age = 25
-    user.verified = True
+def test_foreigners_must_be_foreign_fee():
+    # user.address = paris_address
+    # user.age = 25
+    # user.verified = True
+
+    user = ConcreteUserBuilder()
+    user.address_france()
+    user = user.user
 
     assert Shop.must_pay_foreign_fee(user)
