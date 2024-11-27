5aa120e305ce273c61baf6713c1c0b57b95a1ea7
Jules <jules.davoust@yahoo.fr>
2024-11-27 10:32:26 +0100

    build pattern, persona, prod, v2, compilation sucessfull

--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -1,50 +1,38 @@
 from shop import Shop, User
 
+user = User(
+        name="bob",
+        email="bob@domain.tld"
+    )
 
 def test_happy_path(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=fsf_address,
-        verified=True,
-    )
+    user.address = fsf_address
+    user.age = 25
+    user.verified = True
 
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
 def test_minors_cannot_order_from_the_shop(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=True,
-    )
+    user.address = fsf_address
+    user.age = 16
+    user.verified = True
 
     assert not Shop.can_order(user)
 
 
 
 def test_cannot_order_if_not_verified(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=fsf_address,
-        verified=False,
-    )
+    user.address = fsf_address
+    user.age = 25
+    user.verified = False
 
     assert not Shop.can_order(user)
 
 def test_foreigners_must_be_foreign_fee(paris_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=paris_address,
-        verified=False,
-    )
+    user.address = paris_address
+    user.age = 25
+    user.verified = True
 
     assert Shop.must_pay_foreign_fee(user)
